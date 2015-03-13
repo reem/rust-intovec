7e3e95d6b714e3883a448c4ce0d53237d7419626
Jonathan Reem <jonathan.reem@gmail.com>
2015-03-13 01:42:41 -0700

    (feat) Add IntoString as well.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
-#![allow(unstable)]
 #![deny(missing_docs)]
 #![deny(warnings)]
 
 //! Convert types into a Vec, avoiding copies when possible.
 
-/// Anything convertible into a Vec with or without copies, but avoiding them if possible.
+use std::borrow::ToOwned;
+
+/// Anything convertible into a Vec with or without copies, but avoiding
+/// them if possible.
 pub trait IntoVec<T> {
     /// Convert Self into a Vec
     #[inline]
@@ -36,5 +38,24 @@ impl IntoVec<u8> for String {
 impl<'a> IntoVec<u8> for &'a str {
     #[inline]
     fn into_vec(self) -> Vec<u8> { self.as_bytes().into_vec() }
+}
+
+/// Anything convertible into a String with or without copies, but avoiding
+/// them if possible.
+pub trait IntoString {
+    /// Convert Self into a String
+    fn into_string(self) -> String;
+}
+
+impl IntoString for String {
+    #[inline]
+    fn into_string(self) -> String { self }
+}
+
+impl<'a> IntoString for &'a str {
+    #[inline]
+    fn into_string(self) -> String {
+        self.to_owned()
+    }
 }
 
